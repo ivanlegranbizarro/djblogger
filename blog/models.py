4e91febe56104b9b727939e6b1a889232b4ea8fb
Iván Legrán <68439095+ivanlegranbizarro@users.noreply.github.com>
2023-03-18 11:34:18 +0100

    Post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,27 @@
+from django.contrib.auth.models import User
+from django.core.validators import MinLengthValidator
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100, unique=True, validators=[
+                             MinLengthValidator(3)])
+    subtitle = models.CharField(max_length=100, unique=True, validators=[
+                                MinLengthValidator(3)])
+    slug = models.SlugField(max_length=100, unique=True, null=True, blank=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='post_author')
+    content = models.TextField(validators=[MinLengthValidator(5)])
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Post, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
