a3c3dc5bf49cd10fb07669e891fc3fc55f9d43b3
Iván Legrán <68439095+ivanlegranbizarro@users.noreply.github.com>
2023-03-20 15:53:55 +0100

    home url

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [
+app_name = 'blog'
 
+urlpatterns = [
+    path('', views.HomeView.as_view(), name='home'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from .models import Post
+
+
+class HomeView(generic.ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
