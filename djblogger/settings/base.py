e385c7048eae645c21acddf2246543f66f92452f
Iván Legrán <68439095+ivanlegranbizarro@users.noreply.github.com>
2023-03-20 16:15:35 +0100

    update template folder

--- djblogger/settings/base.py
+++ djblogger/settings/base.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'djblogger.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR / 'templates',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
